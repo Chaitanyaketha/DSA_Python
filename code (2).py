84757092bd65fcdb611239a95e25d479b5e5b5ac
KETHA DHANA VEERA CHAITANYA <kethadvchaitanya@gmail.com>
2024-01-23 22:51:43 +0530

    Palindrome

---
+++ code (2).py
@@ -0,0 +1,16 @@
+# Palindrome number
+
+def Palindrome(n) :
+    rev = 0
+    temp = n
+    while temp != 0 :
+        r = temp % 10
+        rev = rev * 10 + r
+        temp = temp // 10
+    if (rev == n) :
+        print(True)
+    else :
+        print(False)
+    
+n=int(input())
+Palindrome(n)
