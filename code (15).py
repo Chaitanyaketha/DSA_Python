b76a11a8041b3b1f97aa4b39f2d79914d1632ee2
KETHA DHANA VEERA CHAITANYA <kethadvchaitanya@gmail.com>
2024-01-26 19:29:08 +0530

    Rotate List Anticlockwise by one

---
+++ code (15).py
@@ -0,0 +1,21 @@
+def rotateByone(l):
+    # n = len(l)
+    # x = l[0]
+    # for i in range(1, n):
+    #     l[i - 1] = l[i]
+
+    # l[n - 1] = x
+    p=l[0]
+    for i in range(1,len(l)):
+        l[i-1]=l[i]
+        
+    l[-1]=p
+    
+    
+    ###################################################
+l.append(pop(l[0]))
+print(l)
+
+l = [10, 20, 30, 40]
+rotateByone(l)
+print(l)
